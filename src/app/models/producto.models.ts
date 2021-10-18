8120e56bf4f316ad5adc8b437d98db17f355bfec
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2021-10-17 21:22:43 -0500

    modificacion basica

---
+++ src/app/models/producto.models.ts
@@ -0,0 +1,18 @@
+export class Producto{ 
+
+    nombre:String;   
+    cantidad:number; 
+    departamento:String; 
+
+    constructor(pnombre:String,pcantidad:number,pdepartamento:string){ 
+        this.nombre=pnombre; 
+        this.cantidad=pcantidad;  
+        this.departamento=pdepartamento;
+
+    }
+
+
+
+
+
+}
